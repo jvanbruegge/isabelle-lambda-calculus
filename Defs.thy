9910121acab0fb8fa15b5b8c97496a5d7d29d191
Jan van Brügge <supermanitu@gmail.com>
2020-10-19 16:38:01 +0200

    Add subst_all_types function

--- Defs.thy
+++ Defs.thy
@@ -87,6 +87,9 @@ where
   done
 nominal_termination (eqvt) by lexicographic_order
 
+lemma fresh_subst_term: "\<lbrakk> atom z \<sharp> s ; z = y \<or> atom z \<sharp> t \<rbrakk> \<Longrightarrow> atom z \<sharp> subst_term s y t"
+  by (nominal_induct t avoiding: z y s rule: term.strong_induct) auto
+
 nominal_function subst_type :: "\<tau> \<Rightarrow> tyvar \<Rightarrow> \<tau> \<Rightarrow> \<tau>" where
   "subst_type t a TyUnit = TyUnit"
 | "subst_type t a (TyArrow \<tau>1 \<tau>2) = TyArrow (subst_type t a \<tau>1) (subst_type t a \<tau>2)"
@@ -109,9 +112,9 @@ nominal_function subst_types :: "(tyvar * \<tau>) list \<Rightarrow> \<tau> \<Ri
   by (metis neq_Nil_conv surj_pair)
 nominal_termination (eqvt) by lexicographic_order
 
-nominal_function subst_forall_aux :: "tyvar * \<tau> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" where
-  "subst_forall_aux (a, \<tau>) (TyMono \<tau>') = TyMono (subst_type \<tau> a \<tau>')"
-| "atom b \<sharp> (a, \<tau>) \<Longrightarrow> subst_forall_aux (a, \<tau>) (TyForall b \<sigma>) = TyForall b (subst_forall_aux (a, \<tau>) \<sigma>)"
+nominal_function subst_forall :: "tyvar * \<tau> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" where
+  "subst_forall (a, \<tau>) (TyMono \<tau>') = TyMono (subst_type \<tau> a \<tau>')"
+| "atom b \<sharp> (a, \<tau>) \<Longrightarrow> subst_forall (a, \<tau>) (TyForall b \<sigma>) = TyForall b (subst_forall (a, \<tau>) \<sigma>)"
 proof goal_cases
   case (3 P x)
   then obtain a \<tau> t where "x = ((a, \<tau>), t)" apply (cases x) by fast
@@ -119,60 +122,37 @@ proof goal_cases
 next
   case (6 b a \<tau> \<sigma> b2 a2 \<tau>2 \<sigma>2)
   then show ?case apply (auto simp: eqvt_at_def fresh_Pair) by (smt flip_fresh_fresh)
-qed (auto simp: eqvt_def subst_forall_aux_graph_aux_def)
-nominal_termination (eqvt) by lexicographic_order
-
-lemma fresh_subst_forall_aux: "atom a \<sharp> \<tau> \<Longrightarrow> atom a \<sharp> subst_forall_aux (a, \<tau>) \<sigma>"
-  by (induction "(a, \<tau>)" \<sigma> rule: subst_forall_aux.induct) (auto simp: fresh_subst_type)
-lemma fresh_subst_forall_aux_2: "\<lbrakk> atom a \<sharp> \<tau> ; atom a \<sharp> \<sigma> ; atom (a::tyvar) \<sharp> b \<rbrakk> \<Longrightarrow> atom a \<sharp> subst_forall_aux (b, \<tau>) \<sigma>"
-  by (induction "(b, \<tau>)" \<sigma> rule: subst_forall_aux.induct) (auto simp: fresh_subst_type_2)
-
-nominal_function subst_forall :: "\<tau> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" where
-  "subst_forall _ (TyMono \<tau>) = TyMono \<tau>"
-| "atom a \<sharp> \<tau> \<Longrightarrow> subst_forall \<tau> (TyForall a \<sigma>) = subst_forall_aux (a, \<tau>) \<sigma>"
-proof goal_cases
-  case (3 P x)
-  then obtain \<tau> t where "x = (\<tau>, t)" apply (cases x) by blast
-  then show ?case apply (cases t rule: \<sigma>.strong_exhaust) using 3 by (auto simp: fresh_star_def)
-next
-  case (6 a \<tau> \<sigma> a2 \<tau>2 \<sigma>2)
-  then have fresh_a2: "atom a2 \<sharp> \<tau>" by simp
-
-  obtain c::tyvar where "atom c \<sharp> (\<tau>, \<sigma>, \<sigma>2, a, a2)" using obtain_fresh by blast
-  then have c: "atom c \<sharp> \<tau>" "atom c \<sharp> \<sigma>" "atom c \<sharp> \<sigma>2" "atom c \<sharp> a" "atom c \<sharp> a2" using fresh_Pair by auto
-  then have c2: "atom c \<sharp> subst_forall_aux (a, \<tau>) \<sigma>" "atom c \<sharp> subst_forall_aux (a2, \<tau>) \<sigma>2" using fresh_subst_forall_aux_2 by auto
-
-  from 6 have swap_same: "\<sigma> = (a \<leftrightarrow> a2) \<bullet> \<sigma>2" 
-    by (metis Abs1_eq_iff(3) Nominal2_Base.swap_self \<sigma>.eq_iff(2) flip_def fresh_star_zero prod.sel(2) supp_perm_eq_test)
-
-  have eqvt_a: "(a \<leftrightarrow> c) \<bullet> subst_forall_aux (a, \<tau>) \<sigma> = subst_forall_aux (c, \<tau>) ((a \<leftrightarrow> c) \<bullet> \<sigma>)" using swap_same c by (simp add: 6 flip_fresh_fresh subst_forall_aux.eqvt)
-  have eqvt_a2: "(a2 \<leftrightarrow> c) \<bullet> subst_forall_aux (a2, \<tau>) \<sigma>2 = subst_forall_aux (c, \<tau>) ((a2 \<leftrightarrow> c) \<bullet> \<sigma>2)" using swap_same c
-    by (smt "6"(2) "6"(3) Abs1_eq_iff(3) Pair_eqvt Pair_inject flip_at_simps(2) flip_commute flip_fresh_fresh subst_forall_aux.eqvt)
-
-  have "subst_forall_aux (c, \<tau>) ((a \<leftrightarrow> c) \<bullet> \<sigma>) = subst_forall_aux (c, \<tau>) ((a2 \<leftrightarrow> c) \<bullet> \<sigma>2)" using swap_same 6 c by force
-  then have same_c: "(a \<leftrightarrow> c) \<bullet> subst_forall_aux (a, \<tau>) \<sigma> = (a2 \<leftrightarrow> c) \<bullet> subst_forall_aux (a2, \<tau>) \<sigma>2" using eqvt_a eqvt_a2 by simp
-
-  have flip_fresh_a: "(a \<leftrightarrow> c) \<bullet> subst_forall_aux (a, \<tau>) \<sigma> = subst_forall_aux (a, \<tau>) \<sigma>" using flip_fresh_fresh[OF fresh_subst_forall_aux[OF "6"(1)] c2(1)] .
-  have flip_fresh_a2: "(a2 \<leftrightarrow> c) \<bullet> subst_forall_aux (a2, \<tau>) \<sigma>2 = subst_forall_aux (a2, \<tau>) \<sigma>2" using flip_fresh_fresh[OF fresh_subst_forall_aux[OF fresh_a2] c2(2)] .
-
-  show ?case using same_c flip_fresh_a flip_fresh_a2 6 by force
 qed (auto simp: eqvt_def subst_forall_graph_aux_def)
-
-nominal_function subst_all_types :: "\<tau> list \<Rightarrow> \<sigma> \<Rightarrow> \<tau>" where
-  "subst_all_types _ (TyMono \<tau>) = \<tau>"
-| "subst_all_types xs (TyForall a \<sigma>) = (case xs of
-    [] \<Rightarrow> subst_all_types xs \<sigma>
-  | x#xs \<Rightarrow> (let \<tau> = subst_all_types xs \<sigma> in 
-    subst_type x a \<tau>
-  ))"
-       apply (auto simp: eqvt_def subst_all_types_graph_aux_def split: list.splits)
-  apply (meson \<sigma>.strong_exhaust)
-
-  
 nominal_termination (eqvt) by lexicographic_order
 
-lemma fresh_subst_term: "\<lbrakk> atom z \<sharp> s ; z = y \<or> atom z \<sharp> t \<rbrakk> \<Longrightarrow> atom z \<sharp> subst_term s y t"
-  by (nominal_induct t avoiding: z y s rule: term.strong_induct) auto
+lemma fresh_subst_forall_aux: "atom a \<sharp> \<tau> \<Longrightarrow> atom a \<sharp> subst_forall (a, \<tau>) \<sigma>"
+  by (induction "(a, \<tau>)" \<sigma> rule: subst_forall.induct) (auto simp: fresh_subst_type)
+lemma fresh_subst_forall_aux_2: "\<lbrakk> atom a \<sharp> \<tau> ; atom a \<sharp> \<sigma> ; atom (a::tyvar) \<sharp> b \<rbrakk> \<Longrightarrow> atom a \<sharp> subst_forall (b, \<tau>) \<sigma>"
+  by (induction "(b, \<tau>)" \<sigma> rule: subst_forall.induct) (auto simp: fresh_subst_type_2)
+
+nominal_function subst_all_types :: "\<tau> list \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" where
+  "subst_all_types _ (TyMono \<tau>) = TyMono \<tau>"
+| "subst_all_types [] (TyForall a \<sigma>) = TyForall a \<sigma>"
+| "atom a \<sharp> x \<Longrightarrow> subst_all_types (x#xs) (TyForall a \<sigma>) = subst_all_types xs (subst_forall (a, x) \<sigma>)"
+proof goal_cases
+next
+  case (3 P x)
+  then obtain xs t where p: "x = (xs, t)" by (cases x)
+  then show ?case
+  proof (cases xs)
+    case Nil
+    then show ?thesis apply (cases t rule: \<sigma>.strong_exhaust) using 3 Nil p by (auto simp: fresh_star_def)
+  next
+    case (Cons a list)
+    then show ?thesis apply (cases t rule: \<sigma>.strong_exhaust) using 3 Cons p by (auto simp: fresh_star_def)
+  qed
+next
+  case (9 a x xs \<sigma> a2 x2 xs2 \<sigma>2)
+  then show ?case
+    apply auto
+    by (smt "9"(7) Abs1_eq_iff(3) Pair_eqvt Pair_inject \<sigma>.eq_iff(2) atom_eq_iff flip_at_simps(2) flip_fresh_fresh fresh_at_base(2) fresh_subst_forall_aux fresh_subst_forall_aux_2 subst_forall.eqvt)
+qed (auto simp: eqvt_def subst_all_types_graph_aux_def)
+nominal_termination (eqvt) by lexicographic_order
 
 nominal_function split_vars :: "\<sigma> \<Rightarrow> tyvar list * \<tau>" where
   "split_vars (TyMono \<tau>) = ([], \<tau>)"
