a436a9dc9b682983a1c5e9134b7464d2b6cc328e
Jan van Brügge <supermanitu@gmail.com>
2020-11-19 16:08:10 +0100

    Add higher kinded types

--- Typing_Lemmas.thy
+++ Typing_Lemmas.thy
@@ -5,19 +5,16 @@ begin
 lemma fun_ty_lam: "\<lbrakk> \<Gamma> \<turnstile> e : \<tau>1 \<rightarrow> \<tau>2 ; is_value e \<rbrakk> \<Longrightarrow> \<exists>x e'. e = (\<lambda>x:\<tau>1. e')"
   by (nominal_induct \<Gamma> e "\<tau>1 \<rightarrow> \<tau>2" rule: T.strong_induct) auto
 
-lemma ty_fresh_vars: "\<lbrakk> \<Gamma> \<turnstile>\<^sub>t\<^sub>y \<tau> ; atom (a::tyvar) \<sharp> \<Gamma> \<rbrakk> \<Longrightarrow> atom a \<sharp> \<tau>"
-proof (nominal_induct \<Gamma> \<tau> avoiding: a rule: Ty.strong_induct)
+lemma ty_fresh_vars: "\<lbrakk> \<Gamma> \<turnstile>\<^sub>t\<^sub>y \<tau> : k ; atom (a::tyvar) \<sharp> \<Gamma> \<rbrakk> \<Longrightarrow> atom a \<sharp> \<tau>"
+proof (nominal_induct \<Gamma> \<tau> k avoiding: a rule: Ty.strong_induct)
   case (Ty_Var a \<Gamma>)
   then show ?case using fresh_ineq_at_base fresh_not_isin_tyvar by force
-next
-  case (Ty_Forall a \<Gamma> \<sigma>)
-  then show ?case by (metis \<tau>.fresh(4) binder.supp(2) fresh_Cons fresh_def list.set(1) list.simps(15) supp_at_base)
-qed auto
+qed (auto simp: fresh_Cons)
 
 (* inversion rules for abstractions *)
 lemma T_Abs_Inv:
   assumes a: "\<Gamma> \<turnstile> (\<lambda>x : \<tau>1 . e) : \<tau>" and b: "atom x \<sharp> \<Gamma>"
-  obtains \<tau>2 where "BVar x \<tau>1#\<Gamma> \<turnstile> e : \<tau>2" "\<tau> = (\<tau>1 \<rightarrow> \<tau>2)" "\<Gamma> \<turnstile>\<^sub>t\<^sub>y \<tau>1"
+  obtains \<tau>2 where "BVar x \<tau>1#\<Gamma> \<turnstile> e : \<tau>2" "\<tau> = (\<tau>1 \<rightarrow> \<tau>2)" "\<Gamma> \<turnstile>\<^sub>t\<^sub>y \<tau>1 : \<star>"
 proof (cases rule: T.cases[OF a])
   case (3 x' \<Gamma>' \<tau>1' e' \<tau>2)
   have swap: "(x' \<leftrightarrow> x) \<bullet> e' = e" by (metis "3"(2) Abs1_eq_iff(3) Nominal2_Base.swap_self add_flip_cancel flip_def permute_eq_iff permute_plus term.eq_iff(5))
@@ -40,7 +37,7 @@ qed simp_all
 
 lemma T_Let_Inv:
   assumes a: "\<Gamma> \<turnstile> Let x \<tau>1 e1 e2 : \<tau>" and b: "atom x \<sharp> \<Gamma>"
-  shows "\<Gamma> \<turnstile> e1 : \<tau>1 \<and> BVar x \<tau>1 # \<Gamma> \<turnstile> e2 : \<tau> \<and> \<Gamma> \<turnstile>\<^sub>t\<^sub>y \<tau>1"
+  shows "\<Gamma> \<turnstile> e1 : \<tau>1 \<and> BVar x \<tau>1 # \<Gamma> \<turnstile> e2 : \<tau> \<and> \<Gamma> \<turnstile>\<^sub>t\<^sub>y \<tau>1 : \<star>"
 proof (cases rule: T.cases[OF a])
   case (5 x' \<Gamma>' _ \<tau>1 e2' \<tau>2)
   have swap: "(x' \<leftrightarrow> x) \<bullet> e2' = e2"
@@ -61,14 +58,15 @@ proof (cases rule: T.cases[OF a])
 qed simp_all
 
 lemma T_AbsT_Inv:
-  assumes a: "\<Gamma> \<turnstile> (\<Lambda> a . e) : \<tau>" and b: "atom a \<sharp> \<Gamma>" "atom a \<sharp> \<tau>"
-  obtains \<sigma> where "BTyVar a # \<Gamma> \<turnstile> e : \<sigma>" "\<tau> = TyForall a \<sigma>"
+  assumes a: "\<Gamma> \<turnstile> (\<Lambda> a : k . e) : \<tau>" and b: "atom a \<sharp> \<Gamma>" "atom a \<sharp> \<tau>"
+  obtains \<sigma> where "BTyVar a k # \<Gamma> \<turnstile> e : \<sigma>" "\<tau> = TyForall a k \<sigma>"
 proof (cases rule: T.cases[OF a])
-  case (7 a' \<Gamma>' e' \<sigma>')
+  case (7 a' k' \<Gamma>' e' \<sigma>')
   have swap: "(a \<leftrightarrow> a') \<bullet> e' = e" by (metis (no_types, lifting) "7"(2) Abs1_eq_iff(3) Nominal2_Base.swap_self add_flip_cancel flip_def permute_eq_iff permute_plus term.eq_iff(6))
-  then have x: "BTyVar a # \<Gamma> \<turnstile> e : (a \<leftrightarrow> a') \<bullet> \<sigma>'" by (smt "7"(1) "7"(4) "7"(5) Cons_eqvt T.eqvt b binder.perm_simps(2) flip_at_simps(2) flip_def fresh_PairD(2) swap_fresh_fresh)
-  have "\<tau> = (\<forall>a . (a \<leftrightarrow> a') \<bullet> \<sigma>')"
-    by (metis "7"(3) "7"(5) Ty_Forall Ty_Var \<tau>.fresh(2) \<tau>.fresh(4) \<tau>.perm_simps(4) b(2) binder.fresh(2) flip_at_simps(2) flip_fresh_fresh fresh_Cons fresh_not_isin_tyvar isin.simps(5) ty_fresh_vars)
+  then have x: "BTyVar a k # \<Gamma> \<turnstile> e : (a \<leftrightarrow> a') \<bullet> \<sigma>'"
+    by (smt "7"(1) "7"(2) "7"(4) "7"(5) Cons_eqvt T.eqvt b(1) binder.perm_simps(2) flip_at_simps(2) flip_def no_tyvars_in_kinds swap_fresh_fresh term.eq_iff(6))
+  have "\<tau> = (\<forall>a : k . (a \<leftrightarrow> a') \<bullet> \<sigma>')"
+    by (metis "7"(2) "7"(3) Abs1_eq_iff(3) Nominal2_Base.swap_self \<tau>.eq_iff(5) \<tau>.fresh(5) b(2) empty_iff flip_def insert_iff list.set(1) list.set(2) no_vars_in_ty swap_fresh_fresh term.eq_iff(6))
   then show ?thesis using x that by blast
 qed simp_all
 
